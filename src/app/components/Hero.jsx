ab5e8a4ba2bbe43dc0ee0503e68ca7e9eac8dc1d
Dimi <diba0001@stud.kea.dk>
2024-12-03 15:22:23 +0100

    Added: text-logo for index, link-stykling change

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,27 +1,33 @@
 'use client';
 import Image from 'next/image';
-import HeroImage from '../images/gabriel-benois-unsplash.jpg';
+import IndexHeroImage from '../images/samu-lopez-unsplash.jpg';
+import IndexTextLogo from '../images/foo-fest-text-logo.webp';
 
 const Hero = () => {
   return (
     <div className="relative w-full h-screen">
       <Image
-        src={HeroImage}
-        alt="Hero background"
+        src={IndexHeroImage}
+        alt="Hero background for index"
         layout="fill"
         objectFit="cover"
         priority={true}
         className="z-0"
       />
 
-
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
-      <div className="absolute bottom-0 left-0 w-full text-center text-white z-10">
- <h1 className="text-[clamp(4rem,_10vw,_20rem)] font-bold w-full py-4">
-  FOO FEST
-</h1>
-      </div>
+<div className="absolute bottom-0 left-0 w-full text-center text-white z-10 flex justify-center items-center p-4">
+  <Image
+    src={IndexTextLogo}
+    alt="Text logo for index"
+    width={3000}
+    height={100}
+    priority={true}
+    className="z-10"
+  />
+</div>
+
     </div>
   );
 };
