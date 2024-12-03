c4d68a516d23f474126a5efd5830a4bd08f19c38
Dimi <diba0001@stud.kea.dk>
2024-12-03 14:07:48 +0100

    Added: hero h1 clamp

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
       <div className="absolute bottom-0 left-0 w-full text-center text-white z-10">
-       <h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-[10rem] font-bold w-full py-4">
+ <h1 className="text-[clamp(4rem,_10vw,_20rem)] font-bold w-full py-4">
   FOO FEST
 </h1>
       </div>
