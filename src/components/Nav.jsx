68885a1a65568d70b36a9b9dc5ee742e91369b49
Ester <espi0001@stud.kea.dk>
2024-12-04 13:31:55 +0100

    ændret a tags til Link tags

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 import React, { useState } from "react";
-import Image from 'next/image';
-import Logo from '@/images/foo-fest-logo.webp';
+import Image from "next/image";
+import Logo from "@/images/foo-fest-logo.webp";
+import Link from "next/link";
 
 const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,76 +13,48 @@ const Navigation = () => {
 
   return (
     <nav className="absolute top-0 left-0 w-full flex items-center justify-between p-4 z-50">
-
-      <a
-        href="/"
-        className="relative group text-2xl font-light text-white hover:text-black hidden md:block"
-      >
+      <Link href={"/"} className="relative group text-2xl font-light text-white hover:text-black hidden md:block">
         <Image src={Logo} width={35} alt="logo" />
+      </Link>
 
-      </a>
-
-
-      <a
-        href="#"
-        className="relative group text-2xl font-light text-white"
-      >
+      <Link href={"#"} className="relative group text-2xl font-light text-white">
         Tickets
         <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all duration-300 group-hover:w-full"></span>
-      </a>
+      </Link>
 
-
-      <button
-        onClick={toggleMenu}
-        className="relative group text-2xl font-light text-white"
-      >
+      <button onClick={toggleMenu} className="relative group text-2xl font-light text-white">
         Menu
         <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all duration-300 group-hover:w-full"></span>
       </button>
 
-
       {isMenuOpen && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-20 backdrop-blur-lg flex justify-center items-center z-50"
-          onClick={() => setIsMenuOpen(false)}
-        >
+        <div className="fixed inset-0 bg-black bg-opacity-20 backdrop-blur-lg flex justify-center items-center z-50" onClick={() => setIsMenuOpen(false)}>
           <div onClick={(e) => e.stopPropagation()}>
             <ul className="space-y-3 text-center text-3xl">
               <li>
-                <a
-                  href="#"
-                className="relative group text-white"
-                >
+                <Link href={"/"} className="relative group text-white">
                   Home
-                   <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all duration-300 group-hover:w-full"></span>
-                </a>
+                  <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all duration-300 group-hover:w-full"></span>
+                </Link>
               </li>
               <li>
-                <a
-                  href="/about"
-                 className="relative group text-white"
-                >
+                <Link href={"/lineup"} className="relative group text-white">
+                  Line-Up
+                  <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all duration-300 group-hover:w-full"></span>
+                </Link>
+              </li>
+              <li>
+                <Link href={"/about"} className="relative group text-white">
                   About
-                   <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all duration-300 group-hover:w-full"></span>
-                </a>
+                  <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all duration-300 group-hover:w-full"></span>
+                </Link>
               </li>
+
               <li>
-                <a
-                  href="#"
-               className="relative group text-white"
-                >
-                  Services
-                   <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all duration-300 group-hover:w-full"></span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="relative group text-white"
-                >
+                <Link href={"#"} className="relative group text-white">
                   Contact
-                 <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all duration-300 group-hover:w-full"></span>
-                </a>
+                  <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all duration-300 group-hover:w-full"></span>
+                </Link>
               </li>
             </ul>
           </div>
