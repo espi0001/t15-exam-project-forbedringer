a98663e9fde6f8a984e9f5445a312e1e7aea1f77
Dimi <diba0001@stud.kea.dk>
2024-12-05 12:06:44 +0100

    Added: changes to hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <Image src={IndexHeroImage} alt="Hero background for index" layout="fill" objectFit="cover" priority={true} className="z-0" />
       <spline-viewer className="z-20 absolute" url="https://prod.spline.design/xGywt7pLfv6ZN4uB/scene.splinecode"></spline-viewer>
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-      <div className="z-30 absolute bottom-0 left-0 w-full text-white flex flex-col items-start p-4">
+      <div className="z-10 absolute bottom-10 mb-40 left-0 w-full text-white flex flex-col items-start p-4">
         <Image src={IndexTextLogo} alt="Text logo for index" width={3000} height={100} priority={true} className="z-10" />
         <p className="text-xl font-medium z-10 sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">MARCH 24-28, 2025</p>
       </div>
