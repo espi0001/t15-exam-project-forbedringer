5673a2d27c5cce4aea81d4a926a463d1b8c973a7
Sebastian Elias <seen0001@stud.kea.dk>
2024-12-11 13:17:03 +0100

    ændring af button rounded fra md til full

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"; // Importerer en hjælpefunktion (cn) til at s
 
 // Definerer en cva-funktion til at oprette forskellige knap-variantstile
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0", // Standard klasser for knap-udseende og tilstande
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0", // Standard klasser for knap-udseende og tilstande
   {
     // Konfiguration af varianter for knappen (f.eks. farver, størrelse)
     variants: {
@@ -21,8 +21,8 @@ const buttonVariants = cva(
       size: {
         // De forskellige størrelser, som knappen kan have
         default: "h-9 px-4 py-2", // Standardstørrelse
-        sm: "h-8 rounded-md px-3 text-xs", // Lille størrelse
-        lg: "rounded-md px-6 py-2", // Stor størrelse
+        sm: "h-8 rounded-full px-3 text-xs", // Lille størrelse
+        lg: "rounded-full px-6 py-2", // Stor størrelse
       },
     },
     // Standardværdier, hvis ingen varianter angives
