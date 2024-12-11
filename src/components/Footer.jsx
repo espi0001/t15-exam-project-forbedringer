35b4d24bedb7d1b5f137494bfdf53e26783f7ffc
Sebastian Elias <seen0001@stud.kea.dk>
2024-12-11 13:11:27 +0100

    Foreløbig opdatering af footer layout + styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,19 +8,31 @@ import { Button } from "./ui/Button";
 const Footer = () => {
   return (
     //   mx-[20px] lg:mx-[64px] py-[64px] lg:py-[112px]
-    <footer className="mx-[20px] lg:mx-[64px] py-[64px] flex flex-col lg:flex-row justify-between items-center gap-[40px]">
-      <section className="flex flex-col items-center">
+    <footer className="mx-[20px] lg:mx-[64px] py-[64px] flex flex-col justify-between gap-[10px] font-medium">
+      <section className="flex lg:flex-col justify-between ml-48 mb-12">
         <div className="flex items-center gap-1">
-          <Image src={Logo} width={35} alt="logo" />
-          <h4 className="text-white text-xl m-5 font-semibold">FOO FEST</h4>
+          <Image src={Logo} width={65} alt="logo" />
+          {/* <h4 className="text-white text-xl m-5 font-semibold">FOO FEST</h4> */}
+          <h6 className="mb-4 text-[5rem] font-bold ml-8">Be the first to know!</h6>
         </div>
+        <form className="flex gap-4 w-full">
+          <input type="email" placeholder="Enter your email..." alt="Email entering form-field" className="w-1/4 px-4 py-2 rounded-full border border-gray-300 text-black focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all duration-300" />
+          <Button type="submit" size="lg" alt="Button to click to subscribe with chosen email" variant="secondary" className="focus:ring-2 focus:ring-gray-500">
+            Subscribe
+          </Button>
+        </form>
+      </section>
 
-        <ul className="flex flex-wrap gap-4 justify-center">
+      <hr className="w-full border-t border-gray-300 my-4" />
+
+      <section id="Footer" className="flex justify-between text-[1rem]">
+        <p className="text-[1rem]">© 2024 Foo Fest</p>
+        <ul className="flex flex-wrap gap-4">
           <li>
-            <NavigationLink href={"/Tickets"} text={"Lineup"} />
+            <NavigationLink href={"/Tickets"} text={"Tickets"} />
           </li>
           <li>
-            <NavigationLink href={"/lineup"} text={"Line-up"} />
+            <NavigationLink href={"/lineup"} text={"Lineup"} />
           </li>
           <li>
             <NavigationLink href={"/about"} text={"About"} />
@@ -29,18 +41,6 @@ const Footer = () => {
             <NavigationLink href={"/contact"} text={"Contact"} />
           </li>
         </ul>
-      </section>
-
-      <hr className="w-full border-t border-gray-300 my-4 lg:hidden" />
-
-      <section id="Footer" className="flex flex-col items-center">
-        <h6 className="mb-4 text-center">Subscribe to be the first to know!</h6>
-        <form className="flex gap-4 w-full ">
-          <input type="email" placeholder="Enter your email..." alt="Email entering form-field" className="w-full px-4 py-2 rounded-md border border-gray-300 text-black focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all duration-300" />
-          <Button type="submit" size="lg" alt="Button to click to subscribe with chosen email" variant="secondary" className="focus:ring-2 focus:ring-gray-500">
-            Subscribe
-          </Button>
-        </form>
       </section>
     </footer>
   );
