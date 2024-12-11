91ad383991aa95e19e59ddeb10502a89a8e9e5b5
Dimi <diba0001@stud.kea.dk>
2024-12-11 13:37:10 +0100

    Added: italic to button on show-less/more and pushed picture up

--- src/app/band/[slug]/page.jsx
+++ src/app/band/[slug]/page.jsx
@@ -44,10 +44,19 @@ const Page = async ({ params }) => {
   const bandLogo = importLogo(band.logo);
 
   return (
-    <div className="py-[64px] lg:py-[112px]">
+    <div>
       <div className="relative">
-        <Image src={bandLogo} alt={`${band.name} logo`} className="w-full h-[300px] lg:h-[400px] object-cover bg-center" width={1200} height={400} />
-        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white lg:text-4xl font-bold">{band.name}</h1>
+        {/* Adjust Image Container */}
+        <Image
+          src={bandLogo}
+          alt={`${band.name} logo`}
+          className="w-full h-[300px] lg:h-[400px] object-cover bg-center"
+          width={1200}
+          height={400}
+        />
+        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white lg:text-4xl font-bold">
+          {band.name}
+        </h1>
       </div>
       <section className="mx-[20px] lg:mx-[64px] py-[48px] lg:py-[80px] lg:flex gap-[80px] justify-between">
         <article className="lg:w-3/5 grid gap-4">
@@ -78,8 +87,12 @@ const Page = async ({ params }) => {
             {bandSchedules.length > 0 ? (
               <div>
                 {bandSchedules.map((schedule, index) => (
-                  <p key={index} className="p-4 border rounded shadow-sm single-view-schedule-p">
-                    <strong>{schedule.day.toUpperCase()}</strong>: {schedule.stage} ({schedule.start} - {schedule.end})
+                  <p
+                    key={index}
+                    className="p-4 border rounded shadow-sm single-view-schedule-p"
+                  >
+                    <strong>{schedule.day.toUpperCase()}</strong>: {schedule.stage} ({schedule.start} -{" "}
+                    {schedule.end})
                   </p>
                 ))}
               </div>
