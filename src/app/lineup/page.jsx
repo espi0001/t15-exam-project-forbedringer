00db472566fd729fbf013e0fa4c0ef6189bfb9b0
Dimi <diba0001@stud.kea.dk>
2024-12-12 11:12:22 +0100

    Added: new placeholder & contrast filter

--- src/app/lineup/page.jsx
+++ src/app/lineup/page.jsx
@@ -4,7 +4,7 @@ import { IoFilter } from "react-icons/io5";
 
 import Link from "next/link";
 import { api } from "@/lib/api"; // Importer api her
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import FilterPanel from "@/components/FilterPanel";
 import GSAPTextReveal from "@/components/GSAPTextReveal";
 import gsap from "gsap";
@@ -136,7 +136,7 @@ const Page = () => {
         <section className={`transition-transform duration-300 ${isFiltersOpen ? "opacity-50" : "opacity-100"} grid grid-cols-2 lg:grid-cols-4 gap-4`}>
           {filteredBands.slice(0, visibleCount).map((band) => (
             <div key={band.slug} className="relative w-full h-[300px] bg-gray-200 rounded overflow-hidden transition-transform hover:scale-105 group">
-              <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${band.logo})` }} />
+              <div className="absolute inset-0 bg-cover bg-center brightness-50" style={{ backgroundImage: `url(${band.logo})` }} />
               <div className="absolute inset-0 bg-black bg-opacity-20 flex items-end p-4 group-hover:bg-opacity-0">
                 <Link href={`/band/${band.slug}`} className="absolute text-white text-lg font-bold inset-0 flex items-end p-5">
                   {band.name}
