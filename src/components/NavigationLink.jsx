8efb0965d8721f944ce0a3e5680cf7f5f520069f
Ester <espi0001@stud.kea.dk>
2024-12-13 16:01:13 +0100

    farver ændret til config farver

--- src/components/NavigationLink.jsx
+++ src/components/NavigationLink.jsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 const NavigationLink = ({ href, text, closeMenu }) => {
   return (
-    <Link href={href} onClick={closeMenu} className="text-black relative group">
+    <Link href={href} onClick={closeMenu} className="text-black_color relative group">
       {text}
-      <span className="absolute left-0 bottom-0 w-0 h-[1px] bg-white transition-all duration-300 group-hover:w-full"></span>
+      <span className="absolute left-0 bottom-0 w-0 h-[1px] bg-white_color transition-all duration-300 group-hover:w-full"></span>
     </Link>
   );
 };
