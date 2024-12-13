75c87f5f1a5b40cc1abfb713caad11a96b63e77b
Ester <espi0001@stud.kea.dk>
2024-12-13 17:28:55 +0100

    gsap

--- src/components/HeaderText.jsx
+++ src/components/HeaderText.jsx
@@ -1,7 +1,7 @@
 const HeaderText = ({ h1, text }) => {
   return (
-    <div className="flex flex-col mb-10 items-center text-center text-black_color ">
-      <h1>{h1}</h1>
+    <div className="flex flex-col mb-10 items-center text-center text-black_color animate-slide-up hidden-text z-20 opacity-0">
+      <h1 className="">{h1}</h1>
       <p className="max-w-[65ch]">{text}</p>
     </div>
   );
