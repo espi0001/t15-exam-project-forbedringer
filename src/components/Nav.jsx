a91ea65b54c9c25965f413697941ab04373f5cd1
Ester <espi0001@stud.kea.dk>
2024-12-13 16:00:42 +0100

    farver til variabler fra config

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -36,29 +36,29 @@ const Nav = () => {
         <DateHover />
       </div>
 
-      <button onClick={() => setIsMenuOpen(true)} className="text-2xl font-bold text-white relative group">
+      <button onClick={() => setIsMenuOpen(true)} className="text-2xl font-bold text-white_color relative group">
         MENU
-        <span className="absolute left-0 bottom-0 w-0 h-px bg-white transition-all duration-300 group-hover:w-full" />
+        <span className="absolute left-0 bottom-0 w-0 h-px bg-white_color transition-all duration-300 group-hover:w-full" />
       </button>
 
       <AnimatePresence mode="wait">
         {isMenuOpen && (
-          <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className="fixed right-0 top-0 h-screen w-full md:w-[480px] bg-white p-16 text-black z-60">
+          <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className="fixed right-0 top-0 h-screen w-full md:w-[480px] bg-white_color p-16 text-black_color z-60">
             <button onClick={() => setIsMenuOpen(false)} className="absolute top-8 right-8 w-12 h-12 flex items-center justify-center">
-              <div className="w-8 h-[2px] bg-black rotate-45 absolute" />
-              <div className="w-8 h-[2px] bg-black -rotate-45 absolute" />
+              <div className="w-8 h-[2px] bg-black_color rotate-45 absolute" />
+              <div className="w-8 h-[2px] bg-black_color -rotate-45 absolute" />
             </button>
 
             <div className="flex flex-col justify-between h-full">
               <div>
-                <p className="text-gray-600 uppercase text-sm border-b border-gray-600 pb-4 mb-8">Menu</p>
+                <p className="text-grey_color uppercase text-sm border-b border-grey_color pb-4 mb-8">Menu</p>
                 <div className="flex flex-col gap-3">
                   {menuItems.map((item, index) => (
                     <motion.div key={index} initial={{ x: 80 }} animate={{ x: 0 }} transition={{ delay: 0.1 * index }} className="relative" onMouseEnter={() => setSelectedIndicator(item.href)}>
                       {selectedIndicator === item.href && (
                         <motion.div
                           layoutId="indicator"
-                          className="absolute left-[-30px] top-[50%] w-2.5 h-2.5 bg-black rounded-full -translate-y-1/2"
+                          className="absolute left-[-30px] top-[50%] w-2.5 h-2.5 bg-black_color rounded-full -translate-y-1/2"
                           transition={{
                             type: "spring",
                             stiffness: 350,
@@ -66,7 +66,7 @@ const Nav = () => {
                           }}
                         />
                       )}
-                      <Link href={item.href} onClick={() => setIsMenuOpen(false)} className="text-5xl font-light hover:text-gray-600 transition-colors">
+                      <Link href={item.href} onClick={() => setIsMenuOpen(false)} className="text-5xl font-light hover:text-grey_color transition-colors">
                         {item.title}
                       </Link>
                     </motion.div>
@@ -75,13 +75,13 @@ const Nav = () => {
               </div>
 
               {/* Add a line above Instagram and Facebook links */}
-              <p className="text-gray-600 uppercase text-sm border-b border-gray-600 pb-4 mt-20">Follow Us</p>
+              <p className="text-grey_color uppercase text-sm border-b border-grey_color pb-4 mt-20">Follow Us</p>
 
               <div className="flex gap-10 text-sm mb-10">
-                <Link href="#" className="hover:text-gray-600 cursor-pointer">
+                <Link href="#" className="hover:text-grey_color cursor-pointer">
                   Instagram
                 </Link>
-                <Link href="#" className="hover:text-gray-600 cursor-pointer">
+                <Link href="#" className="hover:text-grey_color cursor-pointer">
                   Facebook
                 </Link>
               </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,12 +4,12 @@ export default function Navigation({ activeView, setActiveView }) {
   return (
     <nav className="p-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <h1 className="text-2xl mt-10 font-black text-white">Tickets & overview</h1>
+        <h1 className="text-2xl mt-10 font-black text-white_color">Tickets & overview</h1>
         <div className="space-x-4">
-          <Button className="bg-white hover:bg-[#7d0200] text-black" variant={activeView === "booking" ? "default" : ""} onClick={() => setActiveView("booking")}>
+          <Button className="bg-white_color hover:bg-red_color text-black_color" variant={activeView === "booking" ? "default" : ""} onClick={() => setActiveView("booking")}>
             Book Tickets
           </Button>
-          <Button className="bg-[#7d0200] hover:bg-black text-white" variant={activeView === "schedule" ? "default" : ""} onClick={() => setActiveView("schedule")}>
+          <Button className="bg-red_color hover:bg-black_color text-white_color" variant={activeView === "schedule" ? "default" : ""} onClick={() => setActiveView("schedule")}>
             View Schedule
           </Button>
         </div>
