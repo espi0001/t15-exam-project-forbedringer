31277ffa8c5fa2099f907f53f6eeca287bb83cc3
Ester <espi0001@stud.kea.dk>
2024-12-13 16:14:25 +0100

    farvenavn i footer

--- src/components/FooterNavigationLinks.jsx
+++ src/components/FooterNavigationLinks.jsx
@@ -4,7 +4,7 @@ const FooterNavigationLink = ({ href, text, closeMenu, className }) => {
   return (
     <Link href={href} onClick={closeMenu} className={`relative group ${className}`}>
       {text}
-      <span className="absolute left-0 bottom-0 w-0 h-[1px] bg-white transition-all duration-300 group-hover:w-full"></span>
+      <span className="absolute left-0 bottom-0 w-0 h-[1px] bg-white_color transition-all duration-300 group-hover:w-full"></span>
     </Link>
   );
 };
