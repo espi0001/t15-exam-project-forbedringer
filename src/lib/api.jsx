8cb3964c947eaba43a8512f5e3757ff3a79d2f7a
Sebastian Elias <seen0001@stud.kea.dk>
2024-12-17 13:17:11 +0100

    api base url ændring

--- src/lib/api.jsx
+++ src/lib/api.jsx
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "https://lively-scrawny-secretary.glitch.me";
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://lively-scrawny-secretary.glitch.me";
+// const API_BASE_URL = "http://localhost:8080";
 
 export const api = {
   // GET metoder
