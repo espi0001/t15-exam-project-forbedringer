5f89b97b0e095ff941e42dd8df6f26cebd113664
Ester <espi0001@stud.kea.dk>
2025-01-06 12:25:27 +0100

    ændret til h2 og ændret til p i footer og forsiden

--- src/components/ParallaxAboutComponent.jsx
+++ src/components/ParallaxAboutComponent.jsx
@@ -39,13 +39,13 @@ const ParallaxAboutComponent = () => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-4 md:grid-rows-4 min-h-screen px-[20px] lg:px-[64px] bg-gradient-to-b from-custom-start to-custom-end">
       <div className="text-white md:col-start-1 md:col-end-4 md:row-start-1 md:row-end-3 px-0 py-0 md:py-4 flex flex-col justify-center gap-3">
-        <p
+        <h2
           ref={textRef1}
           className={`text-3xl md:text-6xl font-bold leading-tight transition-all duration-1000
 ${isText1InView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
         >
           Customize your tickets, tents and festival experience now.
-        </p>
+        </h2>
         <Link
           href="/tickets"
           ref={buttonRef1}
@@ -58,13 +58,13 @@ ${isButton1InView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
         </Link>
       </div>
       <div className="text-white md:col-start-2 md:col-end-5 md:row-start-3 md:row-end-5 px-0 py-0 md:py-4 flex flex-col justify-center items-end text-right gap-3">
-        <p
+        <h2
           ref={textRef2}
           className={`text-3xl md:text-6xl font-bold leading-tight transition-all duration-1000
 ${isText2InView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}
         >
           Learn about music, events and a lot more at Foo Fest 2025.
-        </p>
+        </h2>
         <Link
           href="/about"
           ref={buttonRef2}
